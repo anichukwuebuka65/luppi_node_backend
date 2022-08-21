2ea9602b6fc8f19fe2d9b13b47ff765877f206d8
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-08-21 01:04:28 +0100

    fixing errors9

--- database/database.js
+++ database/database.js
@@ -1,12 +1,12 @@
 const {Sequelize, DataTypes} = require('sequelize')
 require("dotenv").config()
 //const  conn = new Sequelize('luppi_app','root','',{host: 'localhost', dialect: 'mysql'})
-const  conn = new Sequelize(process.env.DEV_DATABASE_URL,{
-    // "dialect": "postgres",
-    // "ssl": true,
-    // "dialectOptions": {
+const  conn = new Sequelize(process.env.DATABASE_URL,{
+     "dialect": "postgres",
+     "ssl": true,
+     "dialectOptions": {
          "ssl": true
-    // }
+     }
 })
    
  conn.authenticate()
