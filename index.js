d7109c1d2d0bbd1931a10d33e7c9891a0fea7d69
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-08-25 00:21:24 +0100

    netlify5

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const imagekitAuth = require('./routes/Auth')
 const cookieParser = require('cookie-parser')
 
 
-app.use(cors({origin:"https://super-marzipan-20771a.netlify.app", credentials: true}))
+app.use(cors({origin:"https://scintillating-panda-8cd87c.netlify.app", credentials: true}))
 app.use(connCheck)
 app.use(express.json())
 app.use(cookieParser())
