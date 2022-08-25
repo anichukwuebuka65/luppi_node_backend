26ccf5a229b76b6df019ec5c64bf924741f62880
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-08-25 18:28:23 +0100

    cookie

--- routes/Posts.js
+++ routes/Posts.js
@@ -47,6 +47,7 @@ router.get('/', async(req, res) => {
     try {
         const ids = await fetchIds(req.body.userId, Friends)
         const posts = await fetchPost(ids)
+        console.log(req.cookies)
         res.status(200).json(posts)
     } 
      catch (error) {
