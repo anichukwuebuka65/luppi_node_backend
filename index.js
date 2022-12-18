3cfbea545ae73cd41f2fecc9e30c47ce11bdde9c
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-12-18 22:22:12 +0000

    some changes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const cookieParser = require("cookie-parser");
 app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "https://luppi-5xopynisl-anichukwuebuka65.vercel.app",
+    origin: "https://luppi.vercel.app",
     //origin: "http://localhost:3000",
     credentials: true,
   })
