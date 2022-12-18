bd1b89bbf6ae3c198a410aae8293b3a4412e1e8e
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-12-18 17:37:37 +0000

    changed cors allow access origin

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
-const express = require('express')
-const app = express()
-const registerRouter = require('./routes/Register')
-const loginRouter = require('./routes/Login')
-const logoutRouter = require("./routes/LogOut")
-const {router} = require('./routes/Posts')
-const FriendRequestRouter = require('./routes/FriendRequest')
-const ProfileRouter = require('./routes/Profiles')
-const friendsRouter = require('./routes/Friends')
-const CommentRouter = require('./routes/Comments')
-const UsersRouter = require('./routes/Users')
-const LikeRouter = require('./routes/Likes')
-const authenticateUser = require('./middlewares/authenticateUser')
-const cors = require('cors')
-const connCheck = require('./middlewares/databaseConn')
-require('dotenv').config()
-const imagekitAuth = require('./routes/Auth')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const app = express();
+const registerRouter = require("./routes/Register");
+const loginRouter = require("./routes/Login");
+const logoutRouter = require("./routes/LogOut");
+const { router } = require("./routes/Posts");
+const FriendRequestRouter = require("./routes/FriendRequest");
+const ProfileRouter = require("./routes/Profiles");
+const friendsRouter = require("./routes/Friends");
+const CommentRouter = require("./routes/Comments");
+const UsersRouter = require("./routes/Users");
+const LikeRouter = require("./routes/Likes");
+const authenticateUser = require("./middlewares/authenticateUser");
+const cors = require("cors");
+const connCheck = require("./middlewares/databaseConn");
+require("dotenv").config();
+const imagekitAuth = require("./routes/Auth");
+const cookieParser = require("cookie-parser");
 
-app.set('trust proxy', 1)
-app.use(cors({
-    origin:"https://luppi-react.herokuapp.com", 
+app.set("trust proxy", 1);
+app.use(
+  cors({
+    origin: "https://luppi.vercel.app",
     //origin:"http://localhost:3000",
-    credentials: true}))
-app.use(connCheck)
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
-app.use('/register', registerRouter)
-app.use('/login', loginRouter)
-app.use('/auth', imagekitAuth)
-app.use(authenticateUser)
-app.use('/logout', logoutRouter)
-app.use('/comment', CommentRouter)
-app.use('/users', UsersRouter)
-app.use('/like', LikeRouter)
-app.use('/posts', router)
-app.use('/friends', friendsRouter)
-app.use('/friendrequest',  FriendRequestRouter)
-app.use('/profile', ProfileRouter)
-app.get("*", (req, res)=>{
-    res.status(400).end("invalid route")
-})
-  
-const PORT = process.env.PORT || 5000
-app.listen(PORT,()=>{
-    console.log(`app running on port ${PORT}`)
-})
+    credentials: true,
+  })
+);
+app.use(connCheck);
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use("/register", registerRouter);
+app.use("/login", loginRouter);
+app.use("/auth", imagekitAuth);
+app.use(authenticateUser);
+app.use("/logout", logoutRouter);
+app.use("/comment", CommentRouter);
+app.use("/users", UsersRouter);
+app.use("/like", LikeRouter);
+app.use("/posts", router);
+app.use("/friends", friendsRouter);
+app.use("/friendrequest", FriendRequestRouter);
+app.use("/profile", ProfileRouter);
+app.get("*", (req, res) => {
+  res.status(400).end("invalid route");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
